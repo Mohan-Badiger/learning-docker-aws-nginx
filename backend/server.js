9e849927a55b5d4277421eea6131657d741dd6de
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-08-25 23:05:08 +0530

    First commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const PORT = 5000;
+
+const apiRoutes = require('./routes/api');
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api', apiRoutes)
+
+app.listen(PORT, () => {
+    console.log("server running");
+
+});
