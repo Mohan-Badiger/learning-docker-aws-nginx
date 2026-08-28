755999a04422f7288ad390b7033c7fef3e34aecc
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-08-28 23:40:25 +0530

    feat: initialize React frontend project with Vite and Tailwind CSS integration

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Form = () => {
     return (
-        <div>
-            Form
+        <div className='flex h-screen justify-center items-center'>
+            <div className='flex w-190 h-130'>
+                <div className='w-1/2 border rounded-l border-r-0 border-indigo-500 p-4'>
+                    <div>
+                        <h1 className='font-medium text-2xl'>Welcome User</h1>
+                    </div>
+                </div>
+                <div className=' w-1/2 bg-linear-to-bl from-purple-500 via-indigo-500 to-blue-500'></div>
+            </div>
         </div>
     )
 }
