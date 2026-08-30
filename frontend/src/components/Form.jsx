7aa1f62b76d6155a625aad47f0f7c1732c87ec55
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-08-30 20:39:03 +0530

    feat: added form handling

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -26,36 +26,39 @@ const Form = () => {
     return (
         <div className='flex h-screen justify-center items-center'>
             <div className='flex w-190 h-130'>
-                <div className='w-1/2 border rounded-l border-r-0 border-indigo-500 p-4'>
-                    <div>
-                        <h1 className='font-medium text-2xl mb-6'>Welcome User</h1>
+                <div className='w-1/2  h-full flex items-center border rounded-l border-r-0 border-indigo-500 p-4'>
+                    <div className='w-full'>
+                        <div>
+                            <h1 className='font-medium text-2xl mb-6'>Welcome User</h1>
+                        </div>
+                        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+                            <input
+                                type="email"
+                                name="email"
+                                id="username"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                placeholder="Enter your email"
+                                className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none transition-colors"
+                            />
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                placeholder="Enter Password"
+                                className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none transition-colors"
+                            />
+                            <p>Forget Password</p>
+                            <button
+                                type="submit"
+                                className="w-full py-2 bg-indigo-500 mt-5 text-white rounded font-medium hover:bg-indigo-600 transition-colors cursor-pointer"
+                            >
+                                Login
+                            </button>
+                        </form>
                     </div>
-                    <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-                        <input
-                            type="email"
-                            name="email"
-                            id="username"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="Enter your email"
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none transition-colors"
-                        />
-                        <input
-                            type="password"
-                            name="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            placeholder="Enter Password"
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none transition-colors"
-                        />
-                        <button
-                            type="submit"
-                            className="w-full py-2 bg-indigo-500 text-white rounded font-medium hover:bg-indigo-600 transition-colors cursor-pointer"
-                        >
-                            Login
-                        </button>
-                    </form>
                 </div>
                 <div className=' w-1/2 bg-linear-to-bl from-purple-500 via-indigo-500 to-blue-500'>
                 </div>
